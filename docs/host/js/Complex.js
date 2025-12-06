07185787e032c12acfb629ffd669aa51117069d4
catzgaming2012 <197011726+catzgaming2012@users.noreply.github.com>
2025-12-05 18:56:00 -0700

    static stringify

--- docs/host/js/Complex.js
+++ docs/host/js/Complex.js
@@ -5,21 +5,21 @@ export default class Complex {
     }
 
     add(b) {
-        const that = toComplex(b);
+        const that = Complex.toComplex(b);
 
         this.real += that.real;
         this.imag += that.imag;
         return this;
     }
     subtract(b) {
-        const that = toComplex(b);
+        const that = Complex.toComplex(b);
 
         this.real -= that.real;
         this.imag -= that.imag;
         return this;
     }
     multiply(b) {
-        const that = toComplex(b);
+        const that = Complex.toComplex(b);
 
         const A = (this.real * that.real) - (this.imag * that.imag);
         const B = (this.real * that.imag) + (this.imag * that.real);
@@ -30,7 +30,7 @@ export default class Complex {
         return this;
     }
     divide(b) {
-        const that = toComplex(b);
+        const that = Complex.toComplex(b);
 
         const denominator = (that.real * that.real) + (that.imag * that.imag);
 
@@ -46,56 +46,64 @@ export default class Complex {
 
         return this;
     }
+
+    /**
+    * Returns the distance from 0 + 0i to a complex number
+    */
     abs() {
-        Math.sqrt((this.real * this.real) + (this.imag * this.imag))
+        return Math.sqrt((this.real * this.real) + (this.imag * this.imag))
     }
     conj() {
         this.imag = -this.imag;
         return this;
     }
 
-    toString() {
-        if (this.imag >= 0) {
-            return `${this.real} + ${this.imag}i`;
+    stringify() {
+        if (this.real === 0) {
+            if (this.imag === 0) {
+                return '0';
+            } else {
+                return `${this.imag}i`;
+            }
+        }
+        if (this.imag === 0) {
+            return `${this.real}`;
+        } else if (this.imag > 0) {
+            return `${this.real} + ${Math.abs(this.imag)}i`
         } else {
             return `${this.real} - ${Math.abs(this.imag)}i`
         }
     }
-}
-/**
-* Parses a string into a complex number
-* @param {string} input - The string to convert to a complex number
-I couldn't figure out how to include these in the class :(
-*/
-export function parseComplex(input) {
-    const tokens = [];
 
-    for(let i = 0; i < input.length; i++) {
-        let char = input[i]
+    static parseComplex(input) {
+        const tokens = [];
+
+        for(let i = 0; i < input.length; i++) {
+            let char = input[i]
             
-        if (/\d/.test(char)) {
-            let value = '';
-            while (/\d/.test(char) && i < input.length) {
-                value += char;
-                char = input[++i];
+            if (/\d/.test(char)) {
+                let value = '';
+                while (/\d/.test(char) && i < input.length) {
+                    value += char;
+                    char = input[++i];
+                }
+                tokens.push(Number(value));
+                continue;
             }
-            tokens.push(Number(value));
-            continue;
         }
+        return new Complex(tokens[0], tokens[1])
     }
-    return new Complex(tokens[0], tokens[1])
-}
-
-export function toComplex(num) {
-    if (num instanceof Complex) {
-        return num;
-    } else if (typeof num === "string") {
-        try {
-            parseComplex(num)
-        } catch (error) {
+    static toComplex(num) {
+        if (num instanceof Complex) {
+            return num;
+        } else if (typeof num === "string") {
+            try {
+                parseComplex(num)
+            } catch (error) {
+                return new Complex(Number(num), 0);
+            }
+        } else {
             return new Complex(Number(num), 0);
         }
-    } else {
-        return new Complex(Number(num), 0);
     }
 }
